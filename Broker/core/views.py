0ff3f6c7a8514d38e410abfe50d0c65a248c7551
danielsilva <daniel.silva@av.it.pt>
2016-04-18 01:23:15 +0100

    Added web interface, with views and bootstrap. Added necessary urls to work. Updated requirements.

--- Broker/core/views.py
+++ Broker/core/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+
+def index(request):
+    template = loader.get_template('core/index.html')
+    loggedIn = False
+    if 'firstName' in request.session and 'loggedIn' in request.session:
+        firstName = request.session['firstName']
+        loggedIn = request.session['loggedIn']
+    else:
+        firstName = "Visitante"
+        request.session['firstName'] = firstName
+        request.session['loggedIn'] = False
+
+    context = RequestContext(request, {
+        'firstName': firstName,
+        'loggedIn': loggedIn,
+    })
+    return HttpResponse(template.render(context))
+
+
+def about(request):
+    if 'loggedIn' not in request.session or request.session['loggedIn'] == False or 'username' not in request.session:
+        request.session['firstName'] = "Visitante"
+        request.session['loggedIn'] = False
+
+    template = loader.get_template('core/about.html')
+    return HttpResponse(template.render({'loggedIn': request.session['loggedIn'], 'firstName': request.session['firstName']}))
