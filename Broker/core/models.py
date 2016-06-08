5584d6484c679b57e9acc687b36e4e8b2e8b60cd
Daniel Silva <daniel.silva@av.it.pt>
2016-06-08 15:30:08 +0100

    Allowed services in the same ip addres, but with different ports.

--- Broker/core/models.py
+++ Broker/core/models.py
@@ -29,8 +29,8 @@ class Service(models.Model):
     serviceID = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=250, blank=True)
-    ip = models.CharField(max_length=15, unique=True)
-    port = models.IntegerField()
+    ip = models.CharField(max_length=15)
+    port = models.IntegerField(unique=True)
     createdOn = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
