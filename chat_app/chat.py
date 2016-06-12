963e58255307edb725f46cda295564257f58ef0d
rlebre <ruilebre@ua.pt>
2016-06-12 18:07:05 +0100

    permorfs delete of services when it ends

--- chat_app/chat.py
+++ chat_app/chat.py
@@ -1,45 +1,12 @@
+import urllib2
 import socket
 import threading
+import os
 from time import sleep
 import public_ip
 import json
-import urllib2
 import os
-import random
-
-
-class receiver (threading.Thread):
-    def __init__(self, BUFFER_SIZE):
-        threading.Thread.__init__(self)
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind(('127.0.0.1', 0))
-        self.s.listen(1)
-        self.end = False
-        
-
-    def run(self):
-        print "Listening on port", self.s.getsockname()[1]
-        print "Waiting for connection..."
-        self.conn, self.addr = self.s.accept()
-        print "Running"
-        while not self.end:
-            data = self.conn.recv(BUFFER_SIZE)
-            if not data: break
-            if data =='bye':
-                break
-            else:
-                print "> ", data
-        self.stop()
-
-    def get_port(self):
-        return str(self.s.getsockname()[1])
-
-    def stop(self):
-        #self.s.close()
-        self.end = True
-        delete_service(public_ip.get_lan_ip(), get_port())
-        os._exit(0)
-        
+import requests
 
 class sender (threading.Thread):
     def __init__(self, host, port):
@@ -61,8 +28,52 @@ class sender (threading.Thread):
         return str(self.s.getsockname()[1])
 
     def stop(self):
-        self.running = False
+        URL_delete ='http://localhost:8000/api/services/del/' + str(public_ip.get_lan_ip()) + "/" + str(port)
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+        request = urllib2.Request('http://localhost:8000/api/services/del/' + str(public_ip.get_lan_ip()) + "/" + str(port))
+        request.get_method = lambda: 'DELETE'
+        r = requests.delete(URL_delete, data)
+        #url = opener.open(request)
+        os._exit(0)
+
+
+
+class receiver (threading.Thread):
+    def __init__(self, BUFFER_SIZE):
+        threading.Thread.__init__(self)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind(('127.0.0.1', 0))
+        self.s.listen(1)
+
+    def run(self):
+        print "Listening on port", self.s.getsockname()[1]
+        print "Waiting for connection..."
+        self.conn, self.addr = self.s.accept()
+        print "Running"
+        while 1:
+            data = self.conn.recv(BUFFER_SIZE)
+            if not data:
+                break
+            if data =='bye':
+                break
+            else:
+                print "> ", data
+        self.stop()
+        #os._exit(0)
+
+    def get_port(self):
+        return str(self.s.getsockname()[1])
+
+    def stop(self):
         #self.s.close()
+        URL_delete ='http://localhost:8000/api/services/del/' + str(public_ip.get_lan_ip()) + "/" + str(port)
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+        request = urllib2.Request('http://localhost:8000/api/services/del/' + str(public_ip.get_lan_ip()) + "/" + str(port))
+        request.get_method = lambda: 'DELETE'
+
+        r = requests.delete(URL_delete, data)
+        #url = opener.open(request)
+
         os._exit(0)
 
 
@@ -92,21 +103,21 @@ def delete_service(ip, port):
 
 try:
     BUFFER_SIZE = 20
-    thread1 = receiver(BUFFER_SIZE)
-    thread1.start()
+    thread_receiver = receiver(BUFFER_SIZE)
+    thread_receiver.start()
     sleep(0.1)   
 
-    register_service(public_ip.get_lan_ip(), thread1.get_port())
+    register_service(public_ip.get_lan_ip(), thread_receiver.get_port())
 
     ip = raw_input("Connect to\nIP: ")
     port = int(raw_input("Port: "))
-    thread2 = sender(ip, port)
-    thread2.start()
+    thread_sender = sender(ip, port)
+    thread_sender.start()
     
 except KeyboardInterrupt:
     print "\nStoping"
-    delete_service(public_ip.get_lan_ip(), thread1.get_port())
-    thread1.stop()
-    thread2.stop()
+    delete_service(public_ip.get_lan_ip(), thread_receiver.get_port())
+    thread_receiver.stop()
+    thread_sender.stop()
     os._exit(0)
 
