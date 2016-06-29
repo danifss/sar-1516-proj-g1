d52489613ac2b76f7f32aff8ee6de6f1bf6af231
Daniel Silva <daniel.silva@av.it.pt>
2016-06-29 14:01:10 +0100

    Changes to chat app to use correct IPs. Deleted junk files.

--- socks_ssh/Test.py
+++
@@ -1,8 +0,0 @@
-import socket
-import socks
-import urllib2
-
-socks.set_default_proxy(socks.SOCKS5, "localhost")
-socket.socket = socks.socksocket
-
-print urllib2.urlopen('http://www.google.com').read()
